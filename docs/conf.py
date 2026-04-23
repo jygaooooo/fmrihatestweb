326579761d203570f0009994ae85d49952262255
jygaooooo <140688485+jygaooooo@users.noreply.github.com>
2026-04-23 17:44:23 +0800

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = "Toy RTD Site"
+author = "Your Name"
+copyright = "2026, Your Name"
+release = "0.1.0"
+
+extensions = []
+
+templates_path = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+language = "zh_CN"
+root_doc = "index"
+
+html_theme = "alabaster"
+html_title = "Toy RTD Site"
